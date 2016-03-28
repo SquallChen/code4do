2b2e090cc01d57d7745192a68f47fd25490c19c4
liuyin2004 <liuyin2004@gmail.com>
2016-03-28 09:14:45 +0800

    提交官方组件demo

---
+++ demo/source/view/index.ui.js
@@ -0,0 +1,29 @@
+/***********************************************************************************************************
+ * @Author : nanyuantingfeng
+ **********************************************************************************************************/
+var app, page, nf;
+nf = sm("do_Notification");
+app = sm("do_App");
+page = sm("do_Page");
+
+page.on("back", function(){ app.closePage() });
+ui("action_back").on("touch", function(){ app.closePage() });
+
+/**********************************************************************************************************/
+
+var listview, listdata;
+listview = ui("do_listview");
+listdata = mm("do_ListData");
+listview.bindItems(listdata);
+
+listdata.addData([
+    {NAME : "UI", MSG : "View控件", TYPE : "UI",IMG : "source://image/UI.png"},
+    {NAME : "SM", MSG : "单实例组件", TYPE : "UI",IMG : "source://image/SM.png"},
+    {NAME : "MM", MSG : "多实例组件", TYPE : "UI",IMG : "source://image/MM.png"}
+]);
+listview.refreshItems();;
+
+listview.on("touch", function(index){
+    var data = listdata.getOne(index);
+    app.openPage("source://view/indexC.ui", data);
+});
